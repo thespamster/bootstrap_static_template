ae5313c6f3e61945cf252fadfaee48a1271d2caf
Gavin Brown <iamslowtrain@gmail.com>
2025-02-15 14:51:50 +0000

    update css styl for divs and images. adjust image size to better suit different displays.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM is fully loaded");
     window.scrollTo(0,0);
-
     document.getElementById("press-ham").addEventListener("click", function() {
         console.log('pressed ham');
         document.getElementById("toggle-ham").classList.toggle("d-none");
     });
-    
-
 });
